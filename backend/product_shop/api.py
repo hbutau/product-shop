e251dfca6d5b7748c2027131dd537a4c74822af2
hamub <hbutau35@gmail.com>
2020-11-05 09:46:22 +0200

    smoke test

---
+++ backend/product_shop/api.py
@@ -0,0 +1,7 @@
+from ninja import Router
+
+router = Router()
+
+@router.get('')
+def add(request, a:int, b:int):
+    return {'result': a+b}
---
+++ backend/product_shop/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ProductShopConfig(AppConfig):
+    name = 'product_shop'
