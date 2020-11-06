213c67711abc359c86080590dc339a288e0dd06d
hamub <hbutau35@gmail.com>
2020-11-06 17:39:24 +0200

    initial models

--- backend/product_shop/models.py
+++ backend/product_shop/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=100, blank=False)
+    sku = models.CharField(max_length=100, blank=False)
+    # category = model.
+    tags = models.ForeignKey(Tag, related_name='product_tags', on_delete=models.CASCADE)
+    pass
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_lenght=100)
+           
+
+class Tag(models.Model):
+    name = models.CharField()
