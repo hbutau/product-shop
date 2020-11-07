96ce6d6c50dfbb2b0e7fd57e851166fe8825c5b5
hamub <hbutau35@gmail.com>
2020-11-07 12:56:35 +0200

    model ammendments

--- backend/product_shop/admin.py
+++ backend/product_shop/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
+from .models import Category, Product, Order
+
 # Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    pass
--- backend/product_shop/models.py
+++ backend/product_shop/models.py
@@ -1,17 +1,41 @@
 from django.db import models
 
 
-class Product(models.Model):
-    product_name = models.CharField(max_length=100, blank=False)
-    sku = models.CharField(max_length=100, blank=False)
-    # category = model.
-    tags = models.ForeignKey(Tag, related_name='product_tags', on_delete=models.CASCADE)
-    pass
-
-
 class Category(models.Model):
-    category_name = models.CharField(max_lenght=100)
-           
+    name = models.CharField(max_length=100)
+
+    class Meta():
+        verbose_name_plural = "categories"
+
+    def __str__(self):
+        return self.category_name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, blank=False)
+    sku = models.CharField(max_length=100, blank=False, unique=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='category_products')
+    unit_price = models.FloatField()
+
+    def __str__(self):
+        return self.product_name
+
 
 class Tag(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=100)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+
+
+class Order(models.Model):
+
+    created = models.DateTimeField()
+    paid = models.BooleanField(default=False)
+    total = models.FloatField()
+
+
+class LineItem(models.Model):
+
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="order_items")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="product_items")
+    quantity = models.PositiveIntegerField()
