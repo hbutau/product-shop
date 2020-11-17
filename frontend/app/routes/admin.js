74f594703e7e14baaca57aab8aa1e33e271c5126
hamub <hbutau35@gmail.com>
2020-11-17 09:15:08 +0200

    category listing with array

--- frontend/app/routes/admin.js
+++ frontend/app/routes/admin.js
@@ -1,4 +1,8 @@
 import Route from '@ember/routing/route';
 
 export default class AdminRoute extends Route {
+
+    model() {
+        // return some data we want in the template
+    }
 }
--- frontend/app/routes/admin/categories.js
+++ frontend/app/routes/admin/categories.js
@@ -3,5 +3,16 @@ import Route from '@ember/routing/route';
 export default class AdminCategoriesRoute extends Route {
     model() {
             // return some data we want in the template
+            return [
+                {
+                    id: 1024, 
+                    name: 'Fist Category'
+                },
+
+                {
+                    id: 2048,
+                    name: 'Second Category'
+                }
+            ];
         }
 }
