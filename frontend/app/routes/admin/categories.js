88dcff7d8a069333877ac8280a01c8ca175da725
hamub <hbutau35@gmail.com>
2020-11-17 09:11:36 +0200

    catogory listing with array

--- frontend/app/routes/admin/categories.js
+++ frontend/app/routes/admin/categories.js
@@ -1,4 +1,7 @@
 import Route from '@ember/routing/route';
 
 export default class AdminCategoriesRoute extends Route {
+    model() {
+            // return some data we want in the template
+        }
 }
